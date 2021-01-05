53d0d68cdea325edf2d8ce9c924c79c47c14bcfb
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-05 10:47:23 -0500

    commit tree b1879fc93d1d7b579f61857096ba4b687e607739

---
+++ db/migrate/20210105154614_add_foreign_key_to_rides.rb
@@ -0,0 +1,6 @@
+class AddForeignKeyToRides < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :rides, :passenger, foreign_key: true
+    add_reference :rides, :taxi, foreign_key: true
+  end
+end
